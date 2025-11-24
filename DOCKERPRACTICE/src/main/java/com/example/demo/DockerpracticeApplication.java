9ce348f031b5bf17ccbd967eb09cd3d2ad100b6e
bluepal-akriti-12 <akriti.kumari@bluepal.com>
2025-11-24 11:47:51 +0530

    frrist commit

---
+++ DOCKERPRACTICE/src/main/java/com/example/demo/DockerpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerpracticeApplication.class, args);
+	}
+
+}
